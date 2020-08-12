cda0f77949fe9c243418efebad6b6bbb8311f1b1
Victor <35284183+Victorgpz@users.noreply.github.com>
2020-08-12 21:37:25 +0530

    Update PortScanner.py

--- PortScanner.py
+++ PortScanner.py
@@ -3,47 +3,51 @@
 #Written by Abhijith A
 #Date: 04-08-2020
 #edited by @victorgpz
+#Date: 12-08-2020
 #Still a prototype
+
 
 import threading, socket, sys, time
 from queue import Queue
 from datetime import datetime
 import argparse
+
+print_lock = threading.Lock()
+
+
+
 start=time.time()
-#Arguments Parsing
-ap=argparse.ArgumentParser()
-ap.add_argument("-t","--host",required=True , help="Target ip adderss or hostname ,eg: -t 127.0.0.1 ")
-ap.add_argument("-p","--port",help="Target Port Range ,eg: -p 0-1000")
+
+ap=argparse.ArgumentParser();
+ap.add_argument("-t","--host",required=True,
+help="target ip adderss")
+ap.add_argument("-p","--port",help="port range")
+ap.add_argument("-z","--thread",help="no of threads")
 args=vars(ap.parse_args())
 
-
-queue = Queue()
 open_ports = []
 op=[]
 
-try:
-    if not args["port"]:
-        for i in range(65535):
-         queue.put(i)
-        op.append(1)
-        op.append(65535)
-    else:
-        op=args["port"].split("-")
-        for i in range(int(op[0]),int(op[1])+1):
-            queue.put(i)
-    target=args["host"]
+host = args["host"]
 
-    print("-"*50)
-    print("Scanning started at "+ str(datetime.now()))
-    print("Checking for Open ports from {0} - {1}".format(int(op[0]),int(op[1])))
-    print("-"*50)
-    print("\n")
+if not args["thread"]:
+    thread=100
+else:
+    thread=int(args["thread"])
 
-except:
-    print("Wrong Syntax")
-    print("Syntax: python PortScanner.py -t <ip> -p <port_range>")
-    print("Example: python PortScanner.py -t 127.0.0.1 -p 0-1000")
-    sys.exit()
+if not args["port"]:
+    op.append(1)
+    op.append(65535)
+else:
+    op=args["port"].split("-")
+
+
+print("-"*50)
+print("Scanning started at "+ str(datetime.now()))
+print("Checking for Open ports from {0} - {1}".format(int(op[0]),int(op[1])))
+print("-"*50)
+print("\n")
+
 
 def scan(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -62,61 +66,19 @@ def threader():
         scan(worker)
         q.task_done()
 
-threads = []
+q = Queue()
 
 for x in range(thread):
     t = threading.Thread(target=threader)
     t.daemon = True
     t.start()
 
-       except socket.error:
-            print("\nConnection Problem with Server")
-            print("Check the Connections and try again...")
-            sys.exit()
-
-       except socket.gaierror:
-            print("\nHostname couldn't be resolved")
-            sys.exit()
-
-       except:
-            print("Threading Error")
-            sys.exit()
-
-
-try:
-    for i in range(100):
-        t = threading.Thread(target=run)
-        threads.append(t)
-        t.daemon = True # die when main  thread die
-
-    for t in threads:
-        t.start()
-
-    for thread in threads:
-            thread.join()
-
-
-except KeyboardInterrupt:
-    print("\nProcess Interrupted")
-    exit()
-
-except socket.error:
-    print("\nConnection Problem with Server")
-    print("Check the Connections and try again...")
-    sys.exit()
-
-except socket.gaierror:
-    print("\nHostname couldn't be resolved")
-    sys.exit()
-
-except:
-    print("Threading Error")
-    sys.exit()
-    
+for worker in range(int(op[0]),int(op[1])):
+    q.put(worker)
 end=time.time()
 print("\n")
 print("-"*50)
 print("The open ports are ",open_ports)
 print("Scan Completed in {} seconds ".format(round(end-start,2)))
 print("-"*50)
-sys.exit()
+sys.exit
